d89058ce42ee2f78ee76a52fd4e73663d3359cb8
Chrono <1796453790@qq.com>
2023-10-07 11:57:11 +0800

    方向阴影小改动

--- URP Learn/Assets/CustomRP/Shaders/LitPass.hlsl
+++ URP Learn/Assets/CustomRP/Shaders/LitPass.hlsl
@@ -43,6 +43,13 @@ Varyings LitPassVertex(Attributes input)
 	UNITY_TRANSFER_INSTANCE_ID(input, output);
 	output.positionWS = TransformObjectToWorld(input.positionOS.xyz);
 	output.positionCS = TransformWorldToHClip(output.positionWS);
+
+	#if UNITY_REVERSED_Z
+		output.positionCS.z = min(output.positionCS.z, output.positionCS.w * UNITY_NEAR_CLIP_VALUE); 
+	#else
+		output.positionCS.z = max(output.positionCS.z, output.positionCS.w * UNITY_NEAR_CLIP_VALUE);
+	#endif
+
 	output.normalWS = TransformObjectToWorldNormal(input.normalOS);
 	float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
 	output.baseUV = input.baseUV * baseST.xy + baseST.zw;
